e4f447100c02c0935c1cb41cbce777066b9d6cc8
amlane <twelvexstring@gmail.com>
2020-06-26 13:32:52 -0500

    added icons and light styling

--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -1,8 +1,46 @@
 import React from "react";
 import "../styles/Cards.css";
+import {
+  FiHeart,
+  FiHeadphones,
+  FiMoon,
+  FiSend,
+  FiScissors,
+  FiMusic,
+  FiSun,
+  FiLock,
+  FiGithub,
+  FiAnchor,
+} from "react-icons/fi";
 
 function SingleCard({ card }) {
-  return <div className="single-card">{card}</div>;
+  const displayIconByCard = () => {
+    switch (card) {
+      case 1:
+        return <FiHeart size="3em" />;
+      case 2:
+        return <FiHeadphones size="3em" />;
+      case 3:
+        return <FiMoon size="3em" />;
+      case 4:
+        return <FiSend size="3em" />;
+      case 5:
+        return <FiScissors size="3em" />;
+      case 6:
+        return <FiMusic size="3em" />;
+      case 7:
+        return <FiSun size="3em" />;
+      case 8:
+        return <FiLock size="3em" />;
+      case 9:
+        return <FiGithub size="3em" />;
+      case 0:
+        return <FiAnchor size="3em" />;
+      default:
+        return card;
+    }
+  };
+  return <div className="single-card">{displayIconByCard()}</div>;
 }
 
 export default SingleCard;
