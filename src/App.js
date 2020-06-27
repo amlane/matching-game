9a3cddca5206c097457c6a0127cd7d0a13aee53b
amlane <twelvexstring@gmail.com>
2020-06-27 11:30:24 -0500

    worked on the UX/UI a lil

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ import Cards from "./components/Cards";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Game of Memory</header>
+      <header className="App-header">
+        <div>
+          <span role="img" aria-label="brain">
+            🧠
+          </span>{" "}
+          Memory
+        </div>
+      </header>
       <Cards />
     </div>
   );
